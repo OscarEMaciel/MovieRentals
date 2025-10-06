22e810902cf05a2817eaf7343af7c18f0d174c72
Ezequiel Maciel <emaciel19@gmail.com>
2025-10-06 14:32:43 -0300

    agrego user

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //va a ser numerico y unico
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        public UserRole Role { get; set; }
+        [Required]
+        public required string Password { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+
+    public enum UserRole
+    {
+        Cliente,
+        Admin
+    }
+}
